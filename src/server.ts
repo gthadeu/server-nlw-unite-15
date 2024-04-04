5caeb3fbee0caf59875aa4f865a6d3426691b892
Thadeu <thadeu.qodeless@outlook.com>
2024-04-03 21:05:25 -0300

    create new routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { createEvent } from './routes/create-event';
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { getEvent } from "./routes/get-event";
+import { registerForEvent } from "./routes/register-for-event";
 
 const app = fastify()
 
@@ -8,6 +11,9 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
+app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBadge)
 
 app.listen({port: 3333}).then(() => {
   console.log("Server is running 🚀")
